14eac6efbf8c730b90ce19ea942d4177cb7675b3
Chetas-M <chetasmahajan30@gmail.com>
2026-04-16 20:05:01 +0530

    Update deploy-gamma.sh

--- infra/scripts/deploy-gamma.sh
+++ infra/scripts/deploy-gamma.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-# TODO: Deployment script for gamma
+# Deployment script for gamma
+echo "Deploying shared package to 192.168.1.50..."
+scp -r "../../packages/shared" chetas@192.168.1.50:/home/chetas/axiom/packages/
+
+echo "Deploying node-gamma to 192.168.1.50..."
+scp -r "../../packages/node-gamma" chetas@192.168.1.50:/home/chetas/axiom/packages/
+echo "Done."
